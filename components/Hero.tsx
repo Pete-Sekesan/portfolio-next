8cf32fdc4352a4289a83f5222986529cc7809d78
Peter Sekesan <peter.sekesan@newsday.com>
2022-12-15 16:15:21 -0500

    about section completed, tailwind custom css added to globals

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,7 @@
+import Link from 'next/link'
 import React from 'react'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
+import BackgroundCircles from './BackgroundCircles'
 
 type Props = {}
 
@@ -12,11 +14,23 @@ function Hero({ }: Props) {
     deleteSpeed: 2,
   })
   return (
-    <div>
-      <h1>
-      <span>{text}</span>
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'> 
+      <BackgroundCircles />
+      {/* replace this with next.js image component  */}
+      <img className=' rounded-full h-32 w-32 mx-auto object-cover' src='https://avatars.githubusercontent.com/u/72456128?v=4' alt='' />
+      <div className='z-20'>
+        <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'> Software Engineer</h2>
+        <h1 className=' text-5xl lg:text-6xl font-semibold px-10'>
+      <span className='mr-3'>{text}</span>
         <Cursor cursorColor='#F7AB0A' />
         </h1>
+        <div className='pt-5'>
+          <Link href='#about'><button className='heroButton'>About</button></Link>
+          <Link href='#experience'><button className='heroButton'>Experience</button></Link>
+          <Link href='#skills'><button className='heroButton'>Skills</button></Link>
+          <Link href='#projects'><button className='heroButton'>Projects</button></Link>
+       </div>
+      </div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Hero from '../components/Hero'
 
 export default function Home() {
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen'>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
         <title>Create Next App</title>
         <meta name="Peter Sekesan Portfolio" content="Portfolio created with Next.js` " />
@@ -12,7 +12,7 @@ export default function Home() {
       {/* Header  */}
       <Header/>
       {/* Hero  */}
-      <section id='hero'>
+      <section id='hero' className='snap-center'>
       <Hero/>
       </section>
       {/* About  */}
