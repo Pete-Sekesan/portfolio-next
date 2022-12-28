6370eea4d40c3b86f49da5d94e3603d4841c74be
Peter Sekesan <peter.sekesan@newsday.com>
2022-12-28 13:08:48 -0500

    background

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -1,26 +1,28 @@
-import React from 'react'
-import { motion } from 'framer-motion'
+import React from 'react';
+import { motion } from 'framer-motion';
 
-type Props = {}
+type Props = {};
 
 function BackgroundCircles({}: Props) {
   return (
-      <motion.div
-          initial={{
-          opacity: 0,
-      }} animate={{
-          scale: [1, 1, 1, 3, 1],
-    opacity:[0.1, 0.2, 0.4, 0.8, 0.1, 1.0]  }}
-          transition={{ duration: 2.5}}
-          className='relative flex justify-center items-center'
-      >
-          <div className='absolute border border-[#333333] rounded-full h-[200px] w-[200px] mt-52 animate-ping'/>
-          <div className='border border-[#333333] rounded-full h-[300px] w-[300px] absolute mt-52 animate-ping' />
-          <div className='border border-[#333333] rounded-full h-[500px] w-[500px] absolute mt-52 animate-ping'/>
-          <div className='rounded-full border border-[#ffffff] opacity-20 h-[650px] w-[650px] absolute mt-52 animate-pulse'/>
-          <div className='border border-[#333333] rounded-full h-[800px] w-[800px] absolute mt-52 animate-ping'/>
+    <motion.div
+      initial={{
+        opacity: 0,
+      }}
+      animate={{
+        scale: [1, 1, 1, 3, 1],
+        opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1.0],
+      }}
+      transition={{ duration: 2.5 }}
+      className='relative flex justify-center items-center'
+    >
+      <div className='absolute border border-[#333333] rounded-full h-[200px] w-[200px] mt-52 animate-ping' />
+      <div className='border border-[#333333] rounded-full h-[300px] w-[300px] absolute mt-52 animate-ping' />
+      <div className='border border-[#333333] rounded-full h-[500px] w-[500px] absolute mt-52 animate-ping' />
+      <div className='rounded-full border border-[#ffffff] opacity-20 h-[700px] w-[700px] absolute mt-52 animate-pulse' />
+      <div className='border border-[#333333] rounded-full h-[800px] w-[800px] absolute mt-52 animate-ping' />
     </motion.div>
-  )
+  );
 }
 
-export default BackgroundCircles
+export default BackgroundCircles;
--- components/Experience.tsx
+++ components/Experience.tsx
@@ -10,9 +10,9 @@ function Experience({}: Props) {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className='h-screen flex relative flex-col overflow-hidden text-left md:flex-row max-w-full px-10 justify-evenly mx-auto scroll-smooth items-center'
+      className='h-screen flex relative flex-col overflow-hidden text-lft md:flex-row max-w-full px-10 justify-evenly mx-auto scroll-smooth items-center'
     >
-      <h3 className='absolute top-32 uppercase tracking-[20px] text-gray-500 text-2xl'>
+      <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
         Experience
       </h3>
       <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory'>
